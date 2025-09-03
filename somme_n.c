77b99bceace6808973c746727c087ddc005b8c12
souadzmani <souadzmani@gmail.com>
2025-09-03 12:08:29 +0100

    Add files via upload

---
+++ somme_n.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+   int n ,s=0, i;
+   printf("entrer un nombre:");
+   scanf("%d",&n);
+
+   for ( i = 0; i <=n; i++)
+   {
+    s=s+i;
+ 
+   }
+      printf("la somme de n nombre est:%d",s);
+   
+
+    
+   
+
+
+
+
+	return 0;
+}
