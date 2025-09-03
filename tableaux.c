bd8f6a4a20b24931cba06e84258199b936ef749d
souadzmani <souadzmani@gmail.com>
2025-09-03 13:59:11 +0100

    Add files via upload

---
+++ tableaux.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int tab[5];
+	tab[0]=1;
+	tab[1]=2;
+	tab[2]=3;
+	tab[3]=4;
+	tab[4]=5;
+	int i=0;
+
+	while (i<6){
+		printf("tab[%d]=%d\n",i,tab[i]);
+		i++;
+	}
+	
+
+
+   
+    
+
+    
+   
+
+
+
+
+	return 0;
+}
