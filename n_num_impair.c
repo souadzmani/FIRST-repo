10129378a48bd01174b309dd137e80b6ad6423bb
souadzmani <souadzmani@gmail.com>
2025-09-03 12:26:50 +0100

    Add files via upload

---
+++ n_num_impair.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n ;
+    printf("entrer un nombre:");
+    scanf("%d",&n);
+    printf("Premiers Nombres Impairssont:" );
+    for (int i = 1; i <= n; i++) {
+        printf("%d\t", (2 * i)-1);
+       
+    }
+    
+
+   
+
+
+    
+   
+
+
+
+
+	return 0;
+}
